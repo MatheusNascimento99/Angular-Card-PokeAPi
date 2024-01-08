a24a6c7deea217e50bd68e387666c501c9ae3017
MatheusNascimento99 <matheuscrnascimento@gmail.com>
2024-01-08 12:51:42 -0300

    finalização de projeto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-  <h1>Aulas de Service</h1>
+  
   <app-card></app-card>
   <router-outlet></router-outlet>`,
   styleUrls: ['./app.component.css']
